49b06ddb79f140a57d860e4cc8da547df1517994
Sreenivas Karthik <bandisreenivakarthik@gmail.com>
2025-10-18 19:44:38 -0400

    Adding spring boot

---
+++ sentimentAnalyzer/src/main/java/com/nexus/SentimentApplicationMain.java
@@ -0,0 +1,11 @@
+package com.nexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentimentApplicationMain {
+  public static void main(String[] args) {
+    SpringApplication.run(SentimentApplicationMain.class, args);
+  }
+}
