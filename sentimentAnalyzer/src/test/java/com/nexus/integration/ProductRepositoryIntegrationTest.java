36e6f35cf40b5a98ceba79d03decc6de5e924e7b
Song <songli@berkeley.edu>
2025-12-03 20:56:51 -0500

    rm redundant unused imports

--- sentimentAnalyzer/src/test/java/com/nexus/integration/ProductRepositoryIntegrationTest.java
+++ sentimentAnalyzer/src/test/java/com/nexus/integration/ProductRepositoryIntegrationTest.java
@@ -1,7 +1,6 @@
 package com.nexus.integration;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
